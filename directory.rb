b8dd631b420d96e58e689f5fa3e0694ecdd101d1
Stefan Liute <stefanliute@gmail.com>
2016-12-05 18:33:24 +0000

    Arrays rewrite

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
-# first we print the list of dastardly students
+# let's put all students into an array
+students = [
+  "Dr. Hannibal Lecter"
+  "Darth Vader"
+  "Nurse Ratched"
+  "Michael Corleone"
+  "Alex DeLarge"
+  "The Wicked Witch of the West"
+  "Terminator"
+  "Freddy Krueger"
+  "The Joker"
+  "Joffrey Baratheon"
+  "Norman Bates"
+]
+# and then print them
 puts "The students of Smokey Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end
 # finally, we print our headcount
-print "Overall, we have #{student_count} great students."
+print "Overall, we have #{students.count} great students."
