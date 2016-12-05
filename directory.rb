f5c57b63e77e9defe84a2a10a470a4096eee7783
Stefan Liute <stefanliute@gmail.com>
2016-12-05 18:23:47 +0000

    Print command + comments added

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of dastardly students
 puts "The students of Smokey Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, we print our headcount
+print "Overall, we have "
+# it's important that print() doesn't add new line characters
+print 11
+print " great students."
