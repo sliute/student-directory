a72ffe1f8cb2f9b842383f3c7bdf6edd055d1f03
Stefan Liute <stefanliute@gmail.com>
2016-12-05 18:56:24 +0000

    Asking for user input rewrite

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+=begin
 # let's put all students into an array
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
@@ -12,6 +13,7 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
 
 def print_header
   puts "The students of Smokey Villains Academy"
@@ -28,7 +30,26 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students."
 end
 
-# nothing happens until we call the methods
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first names
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #gets another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
