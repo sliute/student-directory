77615a5e5e973fb9b3dede7a89ed49a01d979f4b
Stefan Liute <stefanliute@gmail.com>
2016-12-07 12:57:07 +0000

    Challenge 8: Exercises 9-12

---
+++ exercises_08/directory_10.rb
@@ -0,0 +1,46 @@
+def print_header
+  puts "The students of Smokey Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.map { |student| student[:cohort] }.uniq!.each do |cohort|
+    students.select { |student| student[:cohort] == cohort }.each do |student|
+      puts "#{cohort}: #{student[:name]}, " + "hobbies: #{student[:hobbies]}, ".center(20) + "country of birth: #{student[:countryofbirth]}, ".center(25) + "height: #{student[:height]}.".center(15)
+    end
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students."
+end
+
+def input_students
+  puts "Please enter the student's name (hit return), then their respective cohort (hit return)."
+  puts "To finish, just hit return twice in a row."
+  students = []
+  calendar = ['january', 'february', 'march', 'april', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+  # get the first name and use strip instead of chomp
+  name = gets.strip
+  name = "Default Name" if name == ''
+  cohort = gets.strip
+  cohort = "November" if cohort == '' || !calendar.include?(cohort.downcase)
+  #while either the name or the cohort are provided, repeat this code and provide default values for any empty inputs
+  while (name != 'Default Name' || cohort != 'November') do
+    # add the student hash to the array, with the cohort converted to a symbol
+    students << {name: name, cohort: cohort.to_sym, hobbies: :evilmongery, countryofbirth: :Mordor, height: 200}
+    puts "Now we have #{students.count} #{ students.count == 1 ? "student" : "students"}."
+    #gets another name-cohort pair from the user and use gsub with parameters instead of chomp
+    name = gets.gsub(/\n/, '')
+    name = "Default Name" if name == ''
+    cohort = gets.gsub(/\n/, '')
+    cohort = "November" if cohort == '' || !calendar.include?(cohort.downcase)
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
